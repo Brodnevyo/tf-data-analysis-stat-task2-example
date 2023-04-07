ac71871d8ecfa37e396c9a133ab94690774578ff
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-07 18:01:15 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,17 +1,16 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy import stats
 
 
 chat_id = 1025787461 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    n = len(x)
     mean = np.mean(x)
     std = np.std(x, ddof=1)
-    n = len(x)
-    t_left = stats.t.ppf((1 - p) / 2, n - 1)
-    t_right = stats.t.ppf((1 + p) / 2, n - 1)
-    left = mean + t_left * std / np.sqrt(n)
-    right = mean + t_right * std / np.sqrt(n)
-    return left, right
+    t = stats.t.ppf((1 + p) / 2, n - 1)
+    left = mean - t * std / np.sqrt(n)
+    right = mean + t * std / np.sqrt(n)
+    return (left, right)
