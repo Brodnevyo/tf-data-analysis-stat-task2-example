fbfe5a5ac3297cd2c5325030451254f776ac4f74
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-07 18:20:21 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,16 @@
 import pandas as pd
 import numpy as np
 
-from scipy import stats
+from scipy.stats import t
 
 
 chat_id = 1025787461 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
+    df = n - 1
+    s = np.std(x, ddof=1)
+    t_value = t.ppf((1 + p) / 2, df)
+    interval = t_value * s / np.sqrt(n)
     mean = np.mean(x)
-    std = np.std(x, ddof=1)
-    t = stats.t.ppf((1 + p) / 2, n - 1)
-    left = mean - t * std / np.sqrt(n)
-    right = mean + t * std / np.sqrt(n)
-    return (left, right)
+    return (mean - interval, mean + interval)
