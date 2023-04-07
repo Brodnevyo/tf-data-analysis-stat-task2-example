98b2be8e881b3f18b4a5810f23f33b1eed6dafd3
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-07 21:38:44 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+rom scipy import stats
 
 
 chat_id = 1025787461 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
+    x = 2 * x / 14**2
     n = len(x)
-    x_mean = np.median(x)
-    s = np.std(x, ddof=1)
-    t_value = t.ppf((1 + p) / 2, n - 1)
-    margin_of_error = t_value * s / np.sqrt(n)
-    lower_bound = x_mean - margin_of_error
-    upper_bound = x_mean + margin_of_error
-    acceleration_lower_bound = 2 * lower_bound / (14 ** 2)
-    acceleration_upper_bound = 2 * upper_bound / (14 ** 2)
-    return (acceleration_lower_bound, acceleration_upper_bound)
+    mean = np.median(x)
+    std = np.std(x, ddof=1)
+    t = stats.t.ppf((1 + p) / 2, n - 1)
+    lower = mean - t * std / np.sqrt(n)
+    upper = mean + t * std / np.sqrt(n)
+    return (lower, upper)
