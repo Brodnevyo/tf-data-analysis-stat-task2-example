a2c492c9e3f887d1ecc430f02a25fc017449e342
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-07 18:40:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import t
+from scipy.stats import norm
 
 
 chat_id = 1025787461 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    df = n - 1
-    s = np.std(x, ddof=1)
-    t_value = t.ppf((1 + p) / 2, df)
-    interval = t_value * s / np.sqrt(n)
+    std_dev = np.sqrt(2) / np.sqrt(n)
+    z_value = norm.ppf((1 + p) / 2)
+    interval = z_value * std_dev
     mean = np.mean(x)
-    return (mean - interval, mean + interval)
+    distance_interval = (mean - interval, mean + interval)
+    acceleration_interval = tuple(2 * d / 14**2 for d in distance_interval)
+    return acceleration_interval
