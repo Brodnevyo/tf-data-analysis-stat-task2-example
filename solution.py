0f47a69cf4c5e40fb556922a201178d1fc056b4b
Brodnevyo <127347881+Brodnevyo@users.noreply.github.com>
2023-04-07 19:32:49 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,10 +8,16 @@ chat_id = 1025787461 # Ваш chat ID, не меняйте название пе
 
 def solution(p: float, x: np.array) -> tuple:
     n = len(x)
-    std_dev = np.sqrt(2) / np.sqrt(n)
-    z_value = norm.ppf((1 + p) / 2)
-    interval = z_value * std_dev
-    mean = np.mean(x)
-    distance_interval = (mean - interval, mean + interval)
-    acceleration_interval = tuple(2 * d / 14**2 for d in distance_interval)
-    return acceleration_interval
+    num_bootstraps = 10000
+    acceleration_bootstraps = []
+    for i in range(num_bootstraps):
+        x_bootstrap = np.random.choice(x, size=n, replace=True)
+        x_mean_bootstrap = np.mean(x_bootstrap)
+        acceleration_bootstrap = 2 * x_mean_bootstrap / (14 ** 2)
+        acceleration_bootstraps.append(acceleration_bootstrap)
+    acceleration_bootstraps.sort()
+    lower_bound_index = int((1 - p) / 2 * num_bootstraps)
+    upper_bound_index = int((1 + p) / 2 * num_bootstraps)
+    lower_bound = acceleration_bootstraps[lower_bound_index]
+    upper_bound = acceleration_bootstraps[upper_bound_index]
+    return (lower_bound, upper_bound)
